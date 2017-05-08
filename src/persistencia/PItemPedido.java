2b010684b9d5a1569c4e63bef4999717d59c6543
vinnycurso <vinnycaixa@gmail.com>
2017-05-08 00:07:25 -0300

    Esqueleto dos metodos da persistencia item pedido

---
+++ src/persistencia/PItemPedido.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package persistencia;
+
+import entidade.EItemPedido;
+import java.util.List;
+
+/**
+ *
+ * @author vinicius caetano
+ */
+public class PItemPedido {
+
+    public PItemPedido() {
+    }
+
+    public void incluir(EItemPedido itemPedido) {
+    }
+
+    public void alterar(EItemPedido paramentro) {
+    }
+
+    public EItemPedido consultar(int paramentro) {
+        return null;
+    }
+
+    public void excluir(int paramentro) {
+    }
+
+    public List<EItemPedido> listar(EItemPedido itemPedido) {
+        return null;
+    }
+
+}
