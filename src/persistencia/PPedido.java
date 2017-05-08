f79c57ad580d88290b12b6d6068423bbfd6a4fd0
vinnycurso <vinnycaixa@gmail.com>
2017-05-08 00:04:47 -0300

    Esqueleto dos metodos da persistencia pedido

---
+++ src/persistencia/PPedido.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package persistencia;
+
+import entidade.EPedido;
+import java.util.List;
+
+/**
+ *
+ * @author vinicius caetano
+ */
+public class PPedido {
+    
+    public PPedido(){}
+    
+    public void incluir(EPedido pedido){}
+    
+    public void alterar(EPedido paramentro){}
+   
+     public EPedido consultar(int paramentro){
+     return null;
+             }
+     
+     public void excluir(int paramentro){}
+
+     public List<EPedido> listar(EPedido pedido){
+     return null;
+     }
+}
+
