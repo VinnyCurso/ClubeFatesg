3e4bf65635e7140e94d91fb6133cf0ec9efbd08d
aluno <aluno@LAB202-16>
2017-05-08 22:45:36 -0300

    Efetuado a classe negocio

--- src/persistencia/PPedido.java
+++ src/persistencia/PPedido.java
@@ -5,7 +5,13 @@
  */
 package persistencia;
 
+import entidade.EItemPedido;
 import entidade.EPedido;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
 /**
@@ -16,7 +22,45 @@ public class PPedido {
     
     public PPedido(){}
     
-    public void incluir(EPedido pedido){}
+    public void incluir(EPedido pedido) throws SQLException{
+    
+        Connection conection = util.Conexao.getConexao();
+        conection.setAutoCommit(false);
+        
+        try {
+            String sql = "insert into pedido (datapedido, valortotal, codigo_associado) values (now(),?,?)";
+            
+            PreparedStatement prepared = conection.prepareStatement(sql);
+            
+            prepared.setInt(1,0);
+            prepared.setInt(0, 0);
+            prepared.setInt(0, 0);
+            
+            prepared.execute();
+            
+            String sql2 = "select currval ('pedido_codigo_seq') as codigo";
+            
+            Statement st = conection.createStatement();
+            ResultSet resul = st.executeQuery(sql2);
+            
+            if(resul.next()){
+                pedido.setCodigo(resul.getInt("codigo"));
+                
+                
+            }
+            resul.close();
+            
+            PItemPedido pItem = new PItemPedido();
+            
+            for (EItemPedido item : pedido.getListaItens()) {
+               pItem.incluir(item,conection);
+                
+            }
+            conection.commit();
+        } catch (Exception e) {
+            conection.rollback();
+        }
+    }
     
     public void alterar(EPedido paramentro){}
    
